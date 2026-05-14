1c9a3bfc5855e6d7da8b0b6605f2348013835384
junios-study <junios@naver.com>
2026-05-14 16:53:18 +0900

    파일 받기 기능 추가

--- Packet.h
+++ Packet.h
@@ -8,15 +8,27 @@ enum class PacketType
 {
     Move = 0,   // 클라이언트 -> 서버: 이동 방향
     Position,   // 서버 -> 클라이언트: 플레이어 위치
+    C2S_File,   // 파일 요청
+    S2C_File,   // 파일 전송
     Max
 };
 
+//S2C_File 만들때 
+//Data -> 파일 사이즈를 먼저 보내고 그담에 쭈욱보낸다.
+
+//파일 보낸다 -> 잘라서 보내고 -> 파일 다 보냈음
+
+
+
+
 #pragma pack(push, 1)
 
+
 // 공통 헤더: Size(데이터 크기) + Code(패킷 종류)
+// 4 + 2 -> 6byte
 struct PacketHeader
 {
-    unsigned short Size;
+    unsigned long Size;  //2 ^ 32
     unsigned short Code;
 };
 
--- client.cpp
+++ client.cpp
@@ -1,4 +1,5 @@
 ﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <winsock2.h>
 #include <conio.h>
@@ -15,7 +16,7 @@
 //4byte
 void MakePacketHeader(PacketHeader& OutPacketHeader, int DataSize, PacketType Type)
 {
-	OutPacketHeader.Size = htons(DataSize);
+	OutPacketHeader.Size = htonl(DataSize);
 	OutPacketHeader.Code = htons(static_cast<unsigned short>(Type));
 }
 
@@ -55,24 +56,29 @@ int main()
 	SOCKADDR_IN ServerAdress;
 	memset(&ServerAdress, 0, sizeof(ServerAdress));
 	ServerAdress.sin_family = PF_INET;
-	ServerAdress.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ServerAdress.sin_addr.s_addr = inet_addr(SERVER_IP);
 	ServerAdress.sin_port = htons(31000);
 
 	int SendBufferSize = 0;
 	int RecvBufferSize = 0;
 	int BufferSizeLength = sizeof(SendBufferSize);
 
+	RecvBufferSize = 8 * 1024; // 1000 -> 1k, 2^10  1024
+	BufferSizeLength = sizeof(RecvBufferSize);
+
 	//getsockopt(ServerSocket, SOL_SOCKET, SO_SNDBUF, (char*)&SendBufferSize, &BufferSizeLength);
 	//printf("Send Buffer Size: %d\n", SendBufferSize);
 
-	//SendBufferSize = 4;
+	
 	//setsockopt(ServerSocket, SOL_SOCKET, SO_SNDBUF, (char*)&SendBufferSize, BufferSizeLength);
 
-	//getsockopt(ServerSocket, SOL_SOCKET, SO_SNDBUF, (char*)&SendBufferSize, &BufferSizeLength);
-	//printf("Send Buffer Size: %d\n", SendBufferSize);
+	getsockopt(ServerSocket, SOL_SOCKET, SO_RCVBUF, (char*)&RecvBufferSize, &BufferSizeLength);
+	printf("Receive Buffer Size: %d\n", RecvBufferSize);
 
-	//getsockopt(ServerSocket, SOL_SOCKET, SO_RCVBUF, (char*)&RecvBufferSize, &BufferSizeLength);
-	//printf("Receive Buffer Size: %d\n", RecvBufferSize);
+	setsockopt(ServerSocket, SOL_SOCKET, SO_RCVBUF, (char*)&RecvBufferSize, sizeof(RecvBufferSize));
+
+	getsockopt(ServerSocket, SOL_SOCKET, SO_RCVBUF, (char*)&RecvBufferSize, &BufferSizeLength);
+	printf("Receive Buffer Size: %d\n", RecvBufferSize);
 
 	//3way handshake
 	connect(ServerSocket, (SOCKADDR*)&ServerAdress, sizeof(ServerAdress));
@@ -82,33 +88,103 @@ int main()
 	while (true)
 	{
 		PacketHeader Header;
-		MakePacketHeader(Header, sizeof(MoveData), PacketType::Move);
 
 		MoveData Data;
 		Data.Dir = _getch();
 
-		//[][][][] []... []
-		char Buffer[1024] = { 0, };
-		//[][] [][]
-		memcpy(Buffer, &Header, sizeof(Header));
-		memcpy(&Buffer[0] + sizeof(Header), &Data, sizeof(Data));
-
-		SendAll(ServerSocket, Buffer, sizeof(Header) + sizeof(Data));
-
-		
-		//Header
-		//4byte
-		int RecvBytes = recv(ServerSocket, (char*)&Header, sizeof(Header), MSG_WAITALL);
-		Header.Size = ntohs(Header.Size);
-		Header.Code = ntohs(Header.Code);
-
-		switch ((PacketType)(Header.Code))
+		if (Data.Dir == 'F' || Data.Dir == 'f')
 		{
+			//파일 요청
+			//Header
+			//4byte
+			MakePacketHeader(Header, sizeof(MoveData), PacketType::C2S_File);
+
+			//[][][][] []... []
+			char Buffer[1024] = { 0, };
+			//[][][][] [][]
+			memcpy(Buffer, &Header, sizeof(Header));
+			SendAll(ServerSocket, Buffer, sizeof(Header) + sizeof(Data));
+
+			//파일 받기 헤더
+			int RecvBytes = recv(ServerSocket, (char*)&Header, sizeof(Header), MSG_WAITALL);
+			Header.Size = ntohl(Header.Size);
+			Header.Code = ntohs(Header.Code);
+
+			//[][][][][][].. []
+			switch ((PacketType)(Header.Code))
+			{
+			case PacketType::S2C_File:
+
+				//파일 받아서 작성
+				FILE* OutputFile = fopen("카네이션_2.png", "wb");
+
+				int FileSize = Header.Size;
+				int TotalFileWriteSize = 0;
+				char Buffer[512] = { 0, };
+				size_t WriteSize = 0;
+				int Count = 0;
+
+				do
+				{
+					printf("%d\n", ++Count);
+					int RecvBytes = 0;
+					//8192 작은 파일 이면 버퍼만큼 받는게 아니라 
+					if (FileSize - TotalFileWriteSize < sizeof(Buffer))
+					{
+						//받을 사이즈가 버퍼보다 작을경우
+						RecvBytes = recv(ServerSocket, Buffer, FileSize - TotalFileWriteSize, MSG_WAITALL);
+						if (RecvBytes <= 0)
+						{
+							break;
+						}
+					}
+					else
+					{
+						RecvBytes = recv(ServerSocket, Buffer, sizeof(Buffer), MSG_WAITALL);
+						if (RecvBytes <= 0)
+						{
+							break;
+						}
+					}
+
+					WriteSize = fwrite(Buffer, sizeof(char), RecvBytes, OutputFile);
+					TotalFileWriteSize += (int)WriteSize;
+				} while (TotalFileWriteSize < FileSize);
+
+				fclose(OutputFile);
+				break;
+			}
+
+		}
+		else
+		{
+			//이동 요청
+			MakePacketHeader(Header, sizeof(MoveData), PacketType::Move);
+
+
+			//[][][][] []... []
+			char Buffer[1024] = { 0, };
+			//[][] [][]
+			memcpy(Buffer, &Header, sizeof(Header));
+			memcpy(&Buffer[0] + sizeof(Header), &Data, sizeof(Data));
+
+			SendAll(ServerSocket, Buffer, sizeof(Header) + sizeof(Data));
+
+
+			//Header
+			//4byte
+			int RecvBytes = recv(ServerSocket, (char*)&Header, sizeof(Header), MSG_WAITALL);
+			Header.Size = ntohl(Header.Size);
+			Header.Code = ntohs(Header.Code);
+
+			switch ((PacketType)(Header.Code))
+			{
 			case PacketType::Position:
 				ProcessPositionPacket(ServerSocket, Header.Size);
 				break;
+			}
 		}
-		
+
 	}
 
 	closesocket(ServerSocket);
